e4b6f12affe26a47ef31d54b887ed183360a381c
Fatma <fatmaalghannam324@gmail.com>
2024-03-06 13:31:27 +0300

    update

---
+++ mini-project-iOS-bank-api-starter/VC/AccountViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AccountViewController.swift
+//  mini-project-iOS-bank-api-starter
+//
+//  Created by fatma adnan on 06/03/2024.
+//
+
+import UIKit
+
+class AccountViewController: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
